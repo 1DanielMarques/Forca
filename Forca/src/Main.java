d0716fe968bda30aa8b694a7ec18e57c442a39bf
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-29 21:50:33 -0300

    .

--- Forca/src/Main.java
+++ Forca/src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
     public static String letraUsuario = "";
-    public static String palavra="";
+    public static String palavra = "";
     public static List<String> natureza = new ArrayList<String>(Arrays.asList("f,a,u,n,a", "a,r,v,o,r,e", "a,n,i,m,a,l", "r,o,c,h,a", "o,c,e,a,n,o", "p,e,i,x,e", "a,v,e", "o,x,i,g,e,n,i,o", "f,o,t,o,s,s,i,n,t,e,s,e", "b,i,o,d,i,v,e,r,s,i,d,a,d,e"));
     public static List<String> educacao = new ArrayList<String>(Arrays.asList("p,r,o,f,e,s,s,o,r", "a,l,u,n,o", "m,a,t,e,r,i,a,s", "p,r,o,v,a", "t,r,a,b,a,l,h,o", "n,o,t,a", "r,e,c,u,p,e,r,a,c,a,o", "b,o,l,e,t,i,m", "d,i,r,e,t,o,r", "f,a,c,u,l,d,a,d,e"));
     public static List<String> esportes = new ArrayList<String>(Arrays.asList("b,o,l,a", "f,u,t,e,b,o,l", "e,q,u,i,p,e", "b,a,s,q,u,e,t,e", "v,o,l,e,i", "o,l,i,m,p,i,a,d,a", "t,r,e,i,n,a,d,o,r", "t,o,r,c,i,d,a", "c,o,m,p,e,t,i,c,a,o", "n,a,t,a,c,a,o"));
@@ -35,48 +35,71 @@ public class Main {
 
                     sorteiaPalavra(natureza, sorteia());
                     System.out.println("Tamanho palavra: " + tamanhoPalavra);
+                    boolean criada = true;
                     do {
-                        System.out.print("Informe uma letra");
+                        int linhaAux = 7;
+                        int colunaAux = tamanhoPalavra + 5;
+                        String matriz[][] = new String[linhaAux][colunaAux];
+                        if (criada) {
+                            preencheVazio(matriz, linhaAux, colunaAux);
+                            criada = false;
+                        }
+
+                        desenhaMatriz(matriz, linhaAux, colunaAux);
+                        System.out.print("Informe uma letra: ");
                         letraUsuario = sc.next();
+
+
                         verifica = false;
                         for (int i = 0; i < letras.length; i++) {
                             if (letraUsuario.equals(letras[i])) {
-                                verifica = true;
+                                /*A V E a == 0 coluna == 4 + 1
+                                matriz[2][coluna] = letras[i] */
+                                int aux = 4 + i;
+
+                                matriz[2][aux] = letraUsuario;
+                                System.out.println("Valor letra usuario: " + letraUsuario);
+
+                                desenhaMatriz(matriz, linhaAux, colunaAux);
                                 break;
+                            } else {
+                                contador++;
                             }
                         }
-                        if (verifica) {
-                            System.out.println("LETRA CERTA");
-                        } else {
-                            System.out.println("LETRA ERRADA");
-                            contador++;
-                        }
+                    } while (contador <= 10);
 
-                        desenhaMatriz();
-                    } while (contador <= 4);
                     break;
                 case 2:
                     System.out.println(sorteiaPalavra(educacao, sorteia()));
                     System.out.println("Tamanho palavra: " + tamanhoPalavra);
-                    desenhaMatriz();
+                    //desenhaMatriz();
                     break;
                 case 3:
                     System.out.println(sorteiaPalavra(esportes, sorteia()));
                     System.out.println("Tamanho palavra: " + tamanhoPalavra);
-                    desenhaMatriz();
+                    //desenhaMatriz();
                     break;
                 case 4:
                     System.out.println(sorteiaPalavra(tecnologia, sorteia()));
                     System.out.println("Tamanho palavra: " + tamanhoPalavra);
-                    desenhaMatriz();
+                    //desenhaMatriz();
                     break;
                 case 5:
                     System.out.println(sorteiaPalavra(cinema, sorteia()));
                     System.out.println("Tamanho palavra: " + tamanhoPalavra);
-                    desenhaMatriz();
+                    //desenhaMatriz();
                     break;
             }
         } while (opcao != 9);
+    }
+
+    public static void preencheVazio(String matriz[][], int linhaAux, int colunaAux) {
+        System.out.println(colunaAux);
+        for (int i = 0; i < linhaAux; i++) {
+            for (int j = 0; j < colunaAux; j++) {
+                matriz[i][j] = "";
+            }
+        }
     }
 
     public static Integer sorteia() {
@@ -89,7 +112,7 @@ public class Main {
     public static String sorteiaPalavra(List tema, int index) {
         palavra = tema.get(index).toString();
         letras = palavra.split(",");
-        for(int i=0;i<letras.length;i++){
+        for (int i = 0; i < letras.length; i++) {
             System.out.print(letras[i]);
         }
         System.out.println();
@@ -97,54 +120,66 @@ public class Main {
         return tema.get(index).toString();
     }
 
-    public static void desenhaMatriz() {
-        int colunas = 16;
-        System.out.println(colunas);
-        String matriz[][] = new String[7][16];
+    public static void desenhaMatriz(String matriz[][], int linhaAux, int colunaAux) {
+
+        System.out.println(colunaAux);
+        for (int i = 0; i < linhaAux; i++) {
+            for (int j = 0; j < colunaAux; j++) {
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+        }
+
         for (int linha = 0; linha < 1; linha++) {
-            for (int coluna = 0; coluna < colunas; coluna++) {
-                matriz[linha][coluna] = "—";
+            for (int coluna = 1; coluna < colunaAux - 1; coluna++) {
+                matriz[linha][coluna] = "P";
             }
         }
-        for (int linha = 5; linha < 6; linha++) {
-            for (int coluna = 0; coluna < colunas; coluna++) {
-                matriz[linha][coluna] = "—";
+        for (int linha = linhaAux - 1; linha < 7; linha++) {
+            for (int coluna = 1; coluna < colunaAux - 1; coluna++) {
+                matriz[linha][coluna] = "H";
             }
         }
 
-        for (int linha = 1; linha < 5; linha++) {
+        for (int linha = 0; linha < linhaAux; linha++) {
             matriz[linha][0] = "|";
         }
-
-        for (int linha = 1; linha < 5; linha++) {
-            matriz[linha][15] = "|";
+        for (int linha = 1; linha < linhaAux - 1; linha++) {
+            matriz[linha][1] = "X";
+        }
+        //PODE SER
+        for (int coluna = 4; coluna < colunaAux - 1; coluna++) {
+            matriz[5][coluna] = " ";
         }
 
-        for (int linha = 1; linha < 5; linha++) {
-            matriz[linha][1] = "|";
-        }
-        matriz[4][2] = "—";
-        matriz[4][3] = "—";
-        matriz[3][2] = "";
-        matriz[2][2] = "";
-        matriz[2][2] = "Letra:" + String.valueOf(tamanhoPalavra);
-        matriz[1][2] = "";
-        matriz[3][3] = " |";
-        matriz[2][3] = "  ";
-        matriz[1][3] = "  ";
-        matriz[2][3] = "  ";
-        matriz[4][4] = "";
-        matriz[3][4] = "";
-        matriz[2][4] = "";
-        matriz[1][4] = "";
+        matriz[5][2] = "X";
+        matriz[5][3] = "X";
 
-        for (int linha = 1; linha < 5; linha++) {
-            for (int coluna = 4; coluna < 16; coluna++) {
-                matriz[linha][coluna] = "";
-            }
+        //matriz[3][2] = "Letra:" + String.valueOf(tamanhoPalavra);
+       //pode ser isso
+        matriz[3][2] = " ";
+        matriz[3][3] = " ";
+        matriz[1][2] = " ";
+        matriz[1][3] = " ";
+        matriz[2][2] = " ";
+        matriz[2][3] = " ";
+        matriz[4][2] = " ";
+        matriz[4][3] = "X";
+
+
+        for (int i = 4; i < colunaAux - 1; i++) {
+            matriz[1][i] = "_";
         }
-        for (int linha = 5; linha > -1; linha--) {
-            for (int coluna = 0; coluna < colunas; coluna++) {
+
+       /* for (int i = 4; i < colunaAux - 1; i++) {
+            matriz[2][i] = "a";
+        }*/
+        for (int linha = 6; linha > -1; linha--) {
+            matriz[linha][colunaAux - 1] = "|";
+        }
+        // DESENHA A MATRIZ
+        for (int linha = linhaAux - 1; linha > -1; linha--) {
+            for (int coluna = 0; coluna < colunaAux; coluna++) {
                 System.out.print(" " + matriz[linha][coluna]);
             }
             System.out.println();
