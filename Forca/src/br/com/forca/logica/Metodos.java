fd83bf193083e2fdc8cc7cdf5b2b891b97d4648d
mDanielSE <daniel.marques@edu.unifil.br>
2022-09-18 20:24:09 -0300

    Organizando codigo

--- Forca/src/br/com/forca/logica/Metodos.java
+++ Forca/src/br/com/forca/logica/Metodos.java
@@ -4,8 +4,6 @@ import java.util.Random;
 
 public class Metodos {
     Variaveis var = new Variaveis();
-
-    // Método que faz toda a lógica do jogo
     public void forca(String[] tema, String nomeTema) throws InterruptedException {
         for (int i = 0; i < var.letrasErradas.length; i++) {
             var.letrasErradas[i] = "";
@@ -103,8 +101,6 @@ public class Metodos {
         }
         return verificaPalavra;
     }
-
-    // Imprime a matriz atualizada
     public void matrizAtual(String matriz[][], int linhaAux, int colunaAux) {
         for (int linha = linhaAux - 1; linha > -1; linha--) {
             for (int coluna = 0; coluna < colunaAux; coluna++) {
@@ -113,15 +109,11 @@ public class Metodos {
             System.out.println();
         }
     }
-
-    // Sorteia index
     public Integer sorteia() {
         Random gerador = new Random();
         int index = gerador.nextInt(10);
         return index;
     }
-
-    // Sorteia palavra de acordo com o index e o tema
     public void sorteiaPalavra(String[] tema, int index) {
         var.palavra = tema[index];
         var.letras = var.palavra.split(",");
@@ -135,58 +127,45 @@ public class Metodos {
     }
 
     public void desenhaMatriz(String matriz[][], int linhaAux, int colunaAux) {
-
-        // Desenha "_" borda superior
         for (int linha = linhaAux - 1; linha < 7; linha++) {
             for (int coluna = 1; coluna < colunaAux - 1; coluna++) {
                 matriz[linha][coluna] = "=";
             }
         }
-        // Desenha "_" borda inferior
         for (int linha = 0; linha < 1; linha++) {
             for (int coluna = 1; coluna < colunaAux - 1; coluna++) {
                 matriz[linha][coluna] = "=";
             }
         }
-        // Desenha "|" borda esquerda
         for (int linha = 0; linha < linhaAux; linha++) {
             matriz[linha][0] = "│";
         }
-        // Desenha "|" borda direita
         for (int linha = 6; linha > -1; linha--) {
             matriz[linha][colunaAux - 1] = "│";
         }
-        // Desenha vertical forca
         for (int linha = 1; linha < linhaAux - 1; linha++) {
             matriz[linha][1] = "|";
         }
-        // Desenha resto da forca
         for (int linha = 1; linha <= 5; linha++) {
             for (int coluna = 2; coluna <= 3; coluna++) {
-                // Desenha ultimo X vertical forca
                 if (linha == 4 && coluna == 3) {
                     matriz[linha][coluna] = "§";
                 } else if (linha == 5) {
-                    // Desenha 2 X horizontais forca
                     matriz[linha][coluna] = "—";
                 } else {
-                    // Desenha " " embaixo da forca/antes da palavra
                     matriz[linha][coluna] = " ";
                 }
             }
         }
         matriz[3][3] = "O";
-        // Desenha "_" das letras
         for (int i = 4; i < colunaAux - 1; i++) {
             matriz[1][i] = "_";
         }
-        // Desenha " " do lugar das letras e para cima
         for (int linha = 2; linha <= 5; linha++) {
             for (int coluna = 4; coluna < colunaAux - 1; coluna++) {
                 matriz[linha][coluna] = " ";
             }
         }
-        // Imprime a matriz
         for (int linha = linhaAux - 1; linha > -1; linha--) {
             for (int coluna = 0; coluna < colunaAux; coluna++) {
                 System.out.print(" " + matriz[linha][coluna]);
